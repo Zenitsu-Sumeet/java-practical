2d704f2a1e45f4b418e0ba8ca6b839fc37db06e2
Zenitsu-Sumeet <113230293+Zenitsu-Sumeet@users.noreply.github.com>
2023-02-06 11:18:17 +0530

    fghg

--- java 1st practical/sequence.java
+++ java 1st practical/sequence.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class sequence {
     public static void main(String[] args) {
-        for (int i = 0; i < args.length; i++) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for (int i = 0; i < n; i++) {
+            
             
         }
     }
