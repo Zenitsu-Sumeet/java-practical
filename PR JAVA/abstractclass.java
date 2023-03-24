284497345d952ea4fd2a6f18a9d977a5f66f9593
Zenitsu-Sumeet <113230293+Zenitsu-Sumeet@users.noreply.github.com>
2023-03-24 09:27:34 +0530

    bnvbn

---
+++ PR JAVA/abstractclass.java
@@ -0,0 +1,24 @@
+abstract class test{
+    abstract void add();
+}
+class service extends test{
+    void add(){
+        System.out.println("abstract method");
+
+    }
+    public void m1(){
+        System.out.println("method");
+    }
+    public void m2(){
+        System.out.println(" class method 2");
+
+    }
+}
+public class abstractclass {
+    public static void main(String[] args) {
+        service s = new  service();
+        s.add();
+        s.m1();
+        s.m2();
+    }
+}
