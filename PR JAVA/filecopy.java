6c80e030485f62e1e9e9a5220ea465a17088fc4e
Zenitsu-Sumeet <113230293+Zenitsu-Sumeet@users.noreply.github.com>
2023-04-08 11:58:56 +0530

    Update filecopy.java

--- PR JAVA/filecopy.java
+++ PR JAVA/filecopy.java
@@ -1,18 +1,20 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class filecopy {
-    public static void main(String[] args) {
-        try{
-            FileWriter writer = new FileWriter("sumeet");
-            FileReader reader = new FileReader("sumeet");
-            int c;
-            while((c = reader.read())!= -1){
-                System.out.print((char)c);
-            }
-        }catch(IOException i){
-            System.out.println(i);
+    public static void main(String[] args) throws IOException {
+
+        FileInputStream r = new FileInputStream("GTR.txt");
+        FileOutputStream w = new FileOutputStream("copy.txt");
+
+        int c;
+        while ((c = r.read()) != -1) {
+            w.write((char) c);
         }
+        System.out.println("data written");
+
     }
 }
